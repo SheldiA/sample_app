402a873edbc7a20e058f7e361f2888a26c47121a
Ann <banadena@gmail.com>
2013-08-31 21:52:31 +0300

    Add Contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,5 +45,18 @@ describe "StaticPages" do
     end
 
   end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',:text => "RoR Tutorial Sample App | Contact")
+    end
+  end 
    
 end
